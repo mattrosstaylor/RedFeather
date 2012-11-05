31ec0ed3555300746b0e63e81b3887a4c7f3c39b
Matt R Taylor <mrt@ecs.soton.ac.uk>
2012-11-05 01:57:45 +0000

    added json plugin

---
+++ plugins/json.php
@@ -0,0 +1,21 @@
+<?php
+
+$CONF['toolbar']['browse'][] = 'browse_json';
+
+// public function for JSON
+function page_json() {
+        global $DATA;
+
+	header("Content-type: application/json");
+
+	print json_encode($DATA);
+}	
+
+// toolbar item for browse page
+function generate_toolbar_item_browse_json()
+{
+	global $CONF;
+	return '<a href="'.$CONF['script_filename'].'?page=json"><img src="http://icons.iconarchive.com/icons/untergunter/leaf-mimes/16/text-xml-icon.png"/> JSON</a>';
+}
+
+?>
